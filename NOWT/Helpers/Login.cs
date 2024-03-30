004441dd2c5672feb60fa56a52d7356d00c387a8
PWall <34860495+PWall2222@users.noreply.github.com>
2024-03-30 08:57:39 +0100

    fix: change UserAgent to real Firefox

--- NOWT/Helpers/Login.cs
+++ NOWT/Helpers/Login.cs
@@ -184,7 +184,7 @@ public static class Login
 
         if (userAgentTrickery)
         {
-            client.AddDefaultHeader("User-Agent", "Mozilla/5.0");
+            client.AddDefaultHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0");
         }
 
         var request = new RestRequest();
